b3181b21f1a58cb825bdeab5806faa7a4d6b7e58
Rooarii MANUEL <rooarii.manuel@gmail.com>
2020-10-03 22:25:54 +0200

    cow say feature completed

---
+++ index.js
@@ -0,0 +1,9 @@
+const myInformation = require('./information.js');
+myInformation();
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "I'm a moooodule",
+	e : "oO",
+	T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,11 @@
+const info = {
+    name:'Rooarii MANUEL',
+    campus:'Wild Code chool - Toulouse'
+}
+
+const myInfos = () => {
+    console.log(`Hi my name is ${info['name']}, I'm studying web developmentat ${info['campus']}`);
+};
+
+
+module.exports = myInfos;
